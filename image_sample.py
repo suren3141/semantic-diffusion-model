4c1fa00cc50caea26a73faf444aad03e22a0a761
ktzx125 <suren.sritharan@astrazeneca.com>
2024-05-24 15:53:24 +0000

    save condition mask and instance mask

--- image_sample.py
+++ image_sample.py
@@ -10,6 +10,9 @@ import torch as th
 import torch.distributed as dist
 import torchvision as tv
 
+from PIL import Image
+import numpy as np
+
 from guided_diffusion.image_datasets import load_data
 
 from guided_diffusion import dist_util, logger
@@ -19,6 +22,37 @@ from guided_diffusion.script_util import (
     add_dict_to_argparser,
     args_to_dict,
 )
+
+import importlib
+
+
+def visualize_cond(cond, hv_map=False, inst=False, col_map=False, out_path=None):
+    out = []
+    tmp = cond.cpu().numpy().transpose((1, 2, 0))
+    if hv_map:
+        hv_map = tmp[..., :2]
+        tmp = tmp[..., 2:]
+        targets = importlib.import_module('hover_net.models.hovernet.targets')
+        vis_hv_map = getattr(targets, 'vis_hv_map')
+        hv_map = vis_hv_map(hv_map)
+        out.append(hv_map[:, :256, :])
+        out.append(hv_map[:, 256:, :])
+    if col_map:
+        col_map = (tmp[..., :3] +1 ) * 127.5
+        tmp = tmp[..., 3:]
+        out.append(col_map.astype(np.uint8))
+    if inst:
+        edge_map = tmp[..., :1]*255
+        out.append(edge_map.repeat(3, axis=-1).astype(np.uint8))
+
+        tmp = tmp[..., 1:]
+
+    out = np.concatenate(out, axis=1)
+
+    if out_path is not None:
+        Image.fromarray(out).save(out_path)
+
+    return out
 
 
 def main():
@@ -43,12 +77,14 @@ def main():
         batch_size=args.batch_size,
         image_size=args.image_size,
         class_cond=args.class_cond,
-        deterministic=True,
+        deterministic=False,
         random_crop=False,
         random_flip=False,
-        is_train=False,
+        is_train=args.use_train,
         num_classes=args.num_classes,
         use_hv_map=args.use_hv_map,
+        use_col_map=args.use_col_map,
+        preserve_nuclei_col=args.preserve_nuclei_col,
         in_channels=args.in_channels,
     )
 
@@ -60,8 +96,14 @@ def main():
     os.makedirs(image_path, exist_ok=True)
     label_path = os.path.join(args.results_path, 'labels')
     os.makedirs(label_path, exist_ok=True)
+    cond_path = os.path.join(args.results_path, 'cond')
+    os.makedirs(cond_path, exist_ok=True)
     sample_path = os.path.join(args.results_path, 'samples')
     os.makedirs(sample_path, exist_ok=True)
+    if not args.no_instance:
+        inst_path = os.path.join(args.results_path, 'inst_masks')
+        os.makedirs(inst_path, exist_ok=True)
+
 
     logger.log("sampling...")
     all_samples = []
@@ -89,10 +131,19 @@ def main():
         dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
         all_samples.extend([sample.cpu().numpy() for sample in gathered_samples])
 
+        if 'instance' in cond:
+            inst_map = cond['instance'].cpu().numpy().astype(np.uint16)
+            for j in range(inst_map.shape[0]):
+                Image.fromarray(inst_map[j].squeeze()).save(os.path.join(inst_path, cond['path'][j].split('/')[-1].split('.')[0] + '.tif'))        
+
         for j in range(sample.shape[0]):
-            tv.utils.save_image(image[j], os.path.join(image_path, cond['path'][j].split('/')[-1].split('.')[0] + '.png'))
-            tv.utils.save_image(sample[j], os.path.join(sample_path, cond['path'][j].split('/')[-1].split('.')[0] + '.png'))
-            tv.utils.save_image(label[j], os.path.join(label_path, cond['path'][j].split('/')[-1].split('.')[0] + '.png'))
+            img_name = cond['path'][j].split('/')[-1].split('.')[0] + '.png'
+            tv.utils.save_image(image[j], os.path.join(image_path, img_name))
+            tv.utils.save_image(sample[j], os.path.join(sample_path, img_name))
+            tv.utils.save_image(label[j], os.path.join(label_path, img_name))
+
+            visualize_cond(model_kwargs['y'][j], hv_map=args.use_hv_map, inst=not args.no_instance, col_map=args.use_col_map, 
+                           out_path=os.path.join(cond_path, img_name))
 
         logger.log(f"created {len(all_samples) * args.batch_size} samples")
 
@@ -140,6 +191,8 @@ def create_argparser():
         data_dir="",
         dataset_mode="",
         use_hv_map=False,
+        use_col_map=False,
+        preserve_nuclei_col=False,
         clip_denoised=True,
         num_samples=10000,
         batch_size=1,
@@ -147,7 +200,9 @@ def create_argparser():
         model_path="",
         results_path="",
         is_train=False,
-        s=1.0
+        s=1.0,
+        no_instance=False,
+        use_train=False,
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
